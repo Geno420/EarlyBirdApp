c765b9287516d215e04b169d32d95ab4103aad3c
Geno420 <56066010+Geno420@users.noreply.github.com>
2019-12-13 11:23:49 +0200

    Early Chiild Hood App

---
+++ FinalApp/FinalApp/FinalApp.Android/obj/Debug/90/android/src/md59389049606ab8f2c1d58ebb9a3e486cd/SplashActivity.java
@@ -0,0 +1,56 @@
+package md59389049606ab8f2c1d58ebb9a3e486cd;
+
+
+public class SplashActivity
+	extends android.support.v7.app.AppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onResume:()V:GetOnResumeHandler\n" +
+			"";
+		mono.android.Runtime.register ("FinalApp.Droid.SplashActivity, FinalApp.Android", SplashActivity.class, __md_methods);
+	}
+
+
+	public SplashActivity ()
+	{
+		super ();
+		if (getClass () == SplashActivity.class)
+			mono.android.TypeManager.Activate ("FinalApp.Droid.SplashActivity, FinalApp.Android", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public void onResume ()
+	{
+		n_onResume ();
+	}
+
+	private native void n_onResume ();
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ FinalApp/FinalApp/FinalApp/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FinalApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void signInButton(object sender, EventArgs e) 
+        {
+            Navigation.PushAsync(new LoginPage());
+        }
+
+        private void LogInButton(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ButtonPage());
+        }
+    }
+}
